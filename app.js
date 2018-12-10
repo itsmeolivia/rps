3a3a07f79f1c551109c24099a74f32aa76bfa4ed
Olivia Nordquist <olivia@Impa.local>
2018-12-09 23:00:51 -0800

    heroku version

--- app.js
+++ app.js
@@ -9,16 +9,24 @@ const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
 
 
+function convertLetter(letter) {
+  if (letter === 'r') return 'Rock';
+  if (letter === 'p') return 'Paper';
+  if (letter === 's') return 'Scissors';
+}
+
 function win(user, comp) {
   userScore++;
   userScore_span.innerHTML = userScore;
-  result_p.innerHTML = user + ' beats ' + comp + '. You win!';
+  result_p.innerHTML = convertLetter(user) + ' beats ' + convertLetter(comp) + '. You win!';
 }
 
 function lose(user, comp) {
   compScore++;
   compScore_span.innerHTML = compScore;
-  result_p.innerHTML = comp + ' beats ' + user + '. You lose!';
+  result_p.innerHTML = `Rank beats ${convertLetter(user)}. You lose!`;
+  document.getElementById(user).classList.add('red_glow');
+  setTimeout(function() {document.getElementById(user).classList.remove('red_glow');}, 600);
 }
 
 function draw(user, comp) {
@@ -32,21 +40,22 @@ function getCompChoice() {
 
 function game(user_choice) {
   const comp_choice = getCompChoice();
-  switch(user_choice + comp_choice) {
-    case "rs":
-    case "sp":
-    case "pr":
-      win(user_choice, comp_choice);
-      break;
-    case "sr":
-    case "ps":
-    case "rp":
-      lose(user_choice, comp_choice);
-      break;
-    default:
-      draw(user_choice, comp_choice);
-      break;
-  }
+  lose(user_choice, "")
+  // switch(user_choice + comp_choice) {
+  //   case "rs":
+  //   case "sp":
+  //   case "pr":
+  //     win(user_choice, comp_choice);
+  //     break;
+  //   case "sr":
+  //   case "ps":
+  //   case "rp":
+  //     lose(user_choice, comp_choice);
+  //     break;
+  //   default:
+  //     draw(user_choice, comp_choice);
+  //     break;
+  // }
 
 }
 
